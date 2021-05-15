5fd2fba81dc7c989c29741ef57d4d74900f74ba4
Daniel Burgess <daniel@herotechsys.com>
2021-05-15 01:36:26 -0600

    Current v2.0beta script added.

--- AnotherTreasureIsland/_0build_en_script.cmd
+++ AnotherTreasureIsland/_0build_en_script.cmd
@@ -1,12 +1,13 @@
 @echo off
 echo Building English Script... 
 copy /y ".\tools\putt\marvelous\eng_ver2_main\table.tbl" ".\text\en_new\table.tbl"
-copy /y ".\tools\putt\marvelous\eng_ver2_main\main.txt" ".\text\en_new\main.txt"
-".\tools\putt\puttscript.exe" -e ".\text\en_new\table.tbl" ".\text\en_new\main.txt" ".\text\en_new\script.bin"
+copy /y ".\tools\putt\marvelous\eng_ver2_main\main.txt" ".\text\en_new\script.txt"
+".\tools\putt\puttscript.exe" -e ".\text\en_new\table.tbl" ".\text\en_new\script.txt" ".\text\en_new\script.bin"
 echo.
 echo Building English Items...
+copy /y ".\tools\putt\marvelous\eng_ver2_items\table.tbl" ".\text\en_new\items_table.tbl"
 copy /y ".\tools\putt\marvelous\eng_ver2_items\items.txt" ".\text\en_new\items.txt"
-".\tools\putt\puttscript.exe" -e ".\tools\putt\marvelous\eng_ver2_items\table.tbl" ".\tools\putt\marvelous\eng_ver2_items\main.txt" ".\text\en_new\items.bin"
+".\tools\putt\puttscript.exe" -e ".\text\en_new\items_table.tbl" ".\text\en_new\items.txt" ".\text\en_new\items.bin"
 echo.
 echo Done.
 pause
