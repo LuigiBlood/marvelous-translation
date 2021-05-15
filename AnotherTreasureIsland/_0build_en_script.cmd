caae839579bdaa81885eb56a1ef2062c0840f4ca
Daniel Burgess <daniel@herotechsys.com>
2021-05-15 02:27:56 -0600

    Fixed batch files.

--- AnotherTreasureIsland/_0build_en_script.cmd
+++ AnotherTreasureIsland/_0build_en_script.cmd
@@ -6,7 +6,7 @@ copy /y ".\tools\putt\marvelous\eng_ver2_main\main.txt" ".\text\en_new\script.tx
 echo.
 echo Building English Items...
 copy /y ".\tools\putt\marvelous\eng_ver2_items\table.tbl" ".\text\en_new\items_table.tbl"
-copy /y ".\tools\putt\marvelous\eng_ver2_items\items.txt" ".\text\en_new\items.txt"
+copy /y ".\tools\putt\marvelous\eng_ver2_items\main.txt" ".\text\en_new\items.txt"
 ".\tools\putt\puttscript.exe" -e ".\text\en_new\items_table.tbl" ".\text\en_new\items.txt" ".\text\en_new\items.bin"
 echo.
 echo Done.
--- AnotherTreasureIsland/_copy_script_to_transputt.cmd
+++ AnotherTreasureIsland/_copy_script_to_transputt.cmd
@@ -6,7 +6,7 @@ copy /y ".\text\en_new\table.tbl" ".\tools\putt\marvelous\eng_ver2_main\table.tb
 copy /y ".\text\en_new\main.txt" ".\tools\putt\marvelous\eng_ver2_main\main.txt"
 
 copy /y ".\text\en_new\table.tbl" ".\tools\putt\marvelous\eng_ver2_items\table.tbl"
-copy /y ".\text\en_new\items.txt" ".\tools\putt\marvelous\eng_ver2_items\items.txt"
+copy /y ".\text\en_new\items.txt" ".\tools\putt\marvelous\eng_ver2_items\main.txt"
 echo.
 echo Done.
 pause
