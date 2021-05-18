b491be22267cbb1f74a3c6ba48280f735666a825
LuigiBlood <luigiblood@gmail.com>
2021-05-19 00:08:08 +0200

    Change ROM Header

--- AnotherTreasureIsland/build_ast_en.asm
+++ AnotherTreasureIsland/build_ast_en.asm
@@ -13,3 +13,21 @@ include "./asm/insert_en.asm"
 include "./asm/code.asm"
 include "./asm/pointers.asm"
 include "./asm/vwf.asm"
+
+//Header
+seekFile(0x7FB0)
+db "01"		//Nintendo
+db "AVRE"	//Game Code
+db $00, $00, $00, $00, $00, $00
+db $00		//Expansion FLASH
+db $00		//Expansion RAM
+db $00		//Special Version
+db $00
+db "MARVELOUS ENG FAN    "
+db $23		//SuperMMC
+db $35		//ROM+SA1+RAM+Battery
+db $0C		//4MB ROM
+db $06		//64KB RAM
+db $01		//North America
+db $33
+db $20		//2.0
