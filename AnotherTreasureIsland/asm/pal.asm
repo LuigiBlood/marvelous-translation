b1db9e5226f09b23b27b20d39c83ecb6aef283cf
LuigiBlood <luigiblood@gmail.com>
2021-05-19 22:03:23 +0200

    Optimize PAL Intro Scroll Speed

--- AnotherTreasureIsland/asm/pal.asm
+++ AnotherTreasureIsland/asm/pal.asm
@@ -1,32 +1,28 @@
 //PAL Conversion
 seekAddr($BF8000)
 
-//Reduce Introduction Scrolling Speed
+//Increase Introduction Scrolling Speed
 pushvar pc
 seekAddr($9FFA69)
 	//lda $76; cmp.w #$0004
 	jsl pal_intro_scrolling; nop
+seekAddr($9FFA70)
+	//stz $76
+	nop; nop
 pullvar pc
 
 pal_intro_scrolling:
-	phx
 	lda $76
-	ldx $35F0
-	cpx.b #3
-	bne +
-	cmp.w #$0004
-	bcc ++
-	stz $35F0
-	plx
-	sec
-	rtl
-
-+;	cmp.w #$0003
+	cmp.w #$0003
 	bcc +
-	inc $35F0
-	plx
-	sec
-	rtl
-+;	clc
-	plx
-	rtl
+	beq +
+	cmp.w #$0006
+	bcc +
+	beq +
+	cmp.w #$0009
+	bcc +
+	beq +
+	cmp.w #$000D
+	bcc +
+	stz $76
++;	rtl
