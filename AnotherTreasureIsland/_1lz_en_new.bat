689a1f13f8e250398cbceb45a8872d7cbe4b8fa7
LuigiBlood <luigiblood@gmail.com>
2021-05-21 17:53:25 +0200

    Seperate Compression between old and new

---
+++ AnotherTreasureIsland/_1lz_en_new.bat
@@ -0,0 +1,7 @@
+".\tools\lc\recomp.exe" "./gfx/en_new/lz_teamname_map.bin" "./gfx/en_new/lz/lz_teamname_map.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/lz_subtitle_gfx.bin" "./gfx/en_new/lz/lz_subtitle_gfx.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/lz_nintendo_gfx.bin" "./gfx/en_new/lz/lz_nintendo_gfx.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/tmp/4D.bin" "./gfx/en_new/lz/4D.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/tmp/4E.bin" "./gfx/en_new/lz/4E.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/tmp/5F.bin" "./gfx/en_new/lz/5F.bin.lz" 0 1 0
+".\tools\lc\recomp.exe" "./gfx/en_new/tmp/63.bin" "./gfx/en_new/lz/63.bin.lz" 0 1 0
