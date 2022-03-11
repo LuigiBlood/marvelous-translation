1f33538779a2a432da8ba701653f82f706ab2a77
LuigiBlood <luigiblood@gmail.com>
2022-03-11 16:23:33 +0100

    Add Build Chore List Image

--- AnotherTreasureIsland/_0build_en_gfx.cmd
+++ AnotherTreasureIsland/_0build_en_gfx.cmd
@@ -20,6 +20,10 @@ echo - patchies_stuck_note.png
 echo - the_monster_bg.png
 ".\tools\conv\superfamiconv.exe" tiles -i ".\gfx\en_new\img\the_monster_bg.png" -p ".\gfx\en_new\img\the_monster_bg.pal" -d ".\gfx\en_new\tmp\the_monster_bg.gfx" -T 128
 ".\tools\conv\superfamiconv.exe" map -i ".\gfx\en_new\img\the_monster_bg.png" -p ".\gfx\en_new\img\the_monster_bg.pal" -t ".\gfx\en_new\tmp\the_monster_bg.gfx" -d ".\gfx\en_new\tmp\the_monster_bg.map" -T 512 -P 7
+echo - chore_list.png
+".\tools\conv\superfamiconv.exe" palette -i ".\gfx\en_new\img\chore_list.png" -d ".\gfx\en_new\tmp\chore_list.pal" -S
+".\tools\conv\superfamiconv.exe" tiles -i ".\gfx\en_new\img\chore_list.png" -p ".\gfx\en_new\tmp\chore_list.pal" -d ".\gfx\en_new\tmp\chore_list.gfx" -T 128
+".\tools\conv\superfamiconv.exe" map -i ".\gfx\en_new\img\chore_list.png" -p ".\gfx\en_new\tmp\chore_list.pal" -t ".\gfx\en_new\tmp\chore_list.gfx" -d ".\gfx\en_new\tmp\chore_list.map" -T 512 -P 7
 echo Building English Graphics Files...
 ".\tools\bass\bass" ".\asm\graphics_en.asm"
 echo Done.
