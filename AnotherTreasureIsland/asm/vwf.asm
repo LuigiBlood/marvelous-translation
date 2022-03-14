12265c695bd852f2080fb9500d60833c478a0e6f
LuigiBlood <luigiblood@gmail.com>
2022-03-14 11:14:44 +0100

    Implement Ch.4 Start Polly dealing with the bird Text Events + Change Script

--- AnotherTreasureIsland/asm/vwf.asm
+++ AnotherTreasureIsland/asm/vwf.asm
@@ -981,67 +981,70 @@ pullvar pc
 //-- Breakpoint $8CE304 - Event Set Script ID?
 //-- Breakpoint $8CE32D - Event Check
 pushvar pc
-seekAddr($038DB9)	//SA-1 / Town Manager and Lunch Box (Script ID: 203)
+seekAddr($038DB9)	//SA-1 / Ch.3 - Town Manager and Lunch Box (Script ID: 203)
 	jsl event_char_detection
 	nop
 	db $90	//BCC
-seekAddr($039424)	//SA-1 / Lunch Box to Husband (0x4AA / Script ID: 398)
+seekAddr($039424)	//SA-1 / Ch.3 - Lunch Box to Husband (0x4AA / Script ID: 398)
 					//This one is odd... It never triggers, and performs the same thing as the next one
 					//Disable it to avoid conflicts
 	rtl 
 	//jsl event_char_detection
 	//nop
 	//db $90	//BCC
-seekAddr($04E6BA)	//SNES / Lunch Box to Husband (0x4AA / Script ID: 398)
+seekAddr($04E6BA)	//SNES / Ch.3 - Lunch Box to Husband (0x4AA / Script ID: 398)
 					//Stop the Eating animation when he asks if you have the key
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-seekAddr($04F264)	//SNES / Mike and Lunch Box (Script ID: 174)
+seekAddr($04F264)	//SNES / Ch.3 - Mike and Lunch Box (Script ID: 174)
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-seekAddr($04F2AA)	//SNES / Mike and Forgotten Doll Choice (Script ID: 175)
+seekAddr($04F2AA)	//SNES / Ch.3 - Mike and Forgotten Doll Choice (Script ID: 175)
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-seekAddr($06E9AC)	//SNES / Boxing Man, after giving the coin (0x181B / Script ID: 2057)
+seekAddr($06E9AC)	//SNES / Ch.3 - Boxing Man, after giving the coin (0x181B / Script ID: 2057)
 					//When Laughing
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-seekAddr($06E9C4)	//SNES / Boxing Man, after giving the coin (0x181B / Script ID: 2057)
+seekAddr($06E9C4)	//SNES / Ch.3 - Boxing Man, after giving the coin (0x181B / Script ID: 2057)
 					//After Laughing
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-//seekAddr($07EB49)
-	//jsl event_char_detection
-	//nop; nop
-	//sep #$20
-	//db $90	//BCC
-//seekAddr($07ED2C)
-	//jsl event_char_detection
-	//nop; nop
-	//sep #$20
-	//db $90	//BCC
-//seekAddr($07EF2B)
-	//jsl event_char_detection
-	//nop; nop
-	//sep #$20
-	//db $90	//BCC?
-seekAddr($13F938)		//SNES / When the baby calms down after giving the doll (0x498 / Script ID: 392)
+seekAddr($07EB49)	//SNES / Ch.4 - When Polly tickles the bird at the start (0x474 - Script ID: 380)
+					//63 - When she becomes angry at the second sentence
 	jsl event_char_detection
 	nop; nop
 	sep #$20
 	db $90	//BCC
-seekAddr($8FAE68)		//SNES / When Ms. Sisko talks to the kids after getting the Crystal Ball [1CA4] (Script ID: 2444)
-						//The moment when she talks about being concerned about your teacher
+seekAddr($07ED2C)	//SNES / Ch.4 - After the bird is gone, when Polly says not to worry (0x471 / Script ID: 379)
+					//FF - After she says she brought 3 giants, she faces you
+	jsl event_char_detection
+	nop; nop
+	sep #$20
+	db $90	//BCC
+seekAddr($07EF2B)	//SNES / Ch.4 - When Polly tickles the bird at the start (0x474 - Script ID: 380)
+					//62 - After she says "Not so easily!", this code is weird
+	jsl event_char_detection
+	nop; nop
+	sep #$20
+	db $B0	//BCS
+seekAddr($13F938)	//SNES / Ch.3 - When the baby calms down after giving the doll (0x498 / Script ID: 392)
+	jsl event_char_detection
+	nop; nop
+	sep #$20
+	db $90	//BCC
+seekAddr($8FAE68)	//SNES / Ch.2 - When Ms. Sisko talks to the kids after getting the Crystal Ball [1CA4] (Script ID: 2444)
+					//The moment when she talks about being concerned about your teacher
 	jsl event_char_detection
 	nop; nop
 	db $90	//BCC
@@ -1049,8 +1052,8 @@ seekAddr($8FAE70)	//This part prevents an accidental trigger (when it's above 0x
 	nop; nop; nop
 	nop; nop
 
-seekAddr($8FAE87)		//SNES / When Ms. Sisko talks to the kids after getting the Crystal Ball (Script ID: 2444)
-						//The moment when she wonders if she has seen you before
+seekAddr($8FAE87)	//SNES / Ch.2 - When Ms. Sisko talks to the kids after getting the Crystal Ball (Script ID: 2444)
+					//The moment when she wonders if she has seen you before
 	jsl event_char_detection
 	nop; nop
 	sep #$20
