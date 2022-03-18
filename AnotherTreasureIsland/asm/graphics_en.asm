91134b3b89ce5661affced397e32de441700b2bb
LuigiBlood <luigiblood@gmail.com>
2022-03-18 01:05:24 +0100

    Use Macros to insert Large Text

--- AnotherTreasureIsland/asm/graphics_en.asm
+++ AnotherTreasureIsland/asm/graphics_en.asm
@@ -13,228 +13,83 @@ macro priority(variable start, variable size) {
 	}
 }
 
+macro tile(file, bpp, id) {
+	evaluate id = {id}
+	//print (4<<{bpp})*{id},"\n"
+	insert {file},(4<<{bpp})*{id},(4<<{bpp})
+}
+
+macro tile16(file, bpp, id) {
+	tile({file},{bpp},({id}*2)+0x00)
+	tile({file},{bpp},({id}*2)+0x10)
+	tile({file},{bpp},({id}*2)+0x01)
+	tile({file},{bpp},({id}*2)+0x11)
+}
+
 //Font Stuff
 output "../gfx/en_new/tmp/font_large.bin"
 fill 0x1E00,0
 origin 0x0000
 //Top Left, Bottom Left, Top Right, Bottom Right
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x00,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x10,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x01,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x11,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x02,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x12,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x03,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x13,0x10
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x00)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x01)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x10)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x11)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x20)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x21)
 
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x20,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x30,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x21,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x31,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x22,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x32,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x23,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x33,0x10
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x02)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x03)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x12)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x13)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x22)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x23)
 
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x40,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x50,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x41,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x51,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x42,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x52,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x43,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x53,0x10
-//--
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x04,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x14,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x05,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x15,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x06,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x16,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x07,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x17,0x10
-
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x24,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x34,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x25,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x35,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x26,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x36,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x27,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x37,0x10
-
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x44,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x54,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x45,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x55,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x46,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x56,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x47,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x57,0x10
-//--
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x08,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x18,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x09,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x19,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x0A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x1A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x0B,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x1B,0x10
-
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x28,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x38,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x29,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x39,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x2A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x3A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x2B,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x3B,0x10
-
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x48,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x58,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x49,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x59,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x4A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x5A,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x4B,0x10
-insert "../gfx/en_new/tmp/large_shreek.gfx",0x10*0x5B,0x10
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x04)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x05)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x14)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x15)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x24)
+tile16("../gfx/en_new/tmp/large_shreek.gfx",2,0x25)
 //--------
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x08,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x18,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x09,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x19,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0B,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1B,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x04)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x05)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x00)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x01)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x02)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x03)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x00,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x10,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x01,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x11,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x02,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x12,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x03,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x13,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x04,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x14,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x05,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x15,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x06,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x16,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x07,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x17,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x10)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x11)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x12)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x13)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x14)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x15)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x20,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x30,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x21,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x31,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x22,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x32,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x23,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x33,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x24,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x34,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x25,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x35,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x26,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x36,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x27,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x37,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x28,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x38,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x29,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x39,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2B,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3B,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x20)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x21)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x22)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x23)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x40,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x50,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x41,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x51,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x42,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x52,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x43,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x53,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x44,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x54,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x45,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x55,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x46,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x56,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x47,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x57,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x06)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x07)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x16)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x17)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x26)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x27)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x0F,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x1F,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x30)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x31)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x32)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x33)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x34)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x35)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x36)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x37)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x2F,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x3F,0x10
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x24)
+tile16("../gfx/en_new/tmp/large_cutitout.gfx",2,0x25)
 
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4F,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5F,0x10
-
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x60,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x70,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x61,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x71,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x62,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x72,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x63,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x73,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x64,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x74,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x65,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x75,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x66,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x76,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x67,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x77,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x68,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x78,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x69,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x79,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6B,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7B,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7C,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7D,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7E,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x6F,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x7F,0x10
-
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x48,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x58,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x49,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x59,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5A,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x4B,0x10
-insert "../gfx/en_new/tmp/large_cutitout.gfx",0x10*0x5B,0x10
 
 //--Intro & Title
 //35 - Graphics - Title Logo
